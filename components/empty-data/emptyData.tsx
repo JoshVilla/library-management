84eb58ba3911eebf240f5cdbce90ac6a4d355677
JoshVilla <villaearljoshua@gmail.com>
2025-03-02 13:58:47 +0800

    add image

--- components/empty-data/emptyData.tsx
+++ components/empty-data/emptyData.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import React from "react";
+import { useTheme } from "next-themes";
 
 const EmptyData = () => {
+  const theme = useTheme();
+  const themeMode = theme.theme;
   return (
     <div className="w-full flex justify-center py-10">
       <div className="text-center">
         <Image
-          src={"/assets/empty-folder.png"}
+          src={themeMode === "dark" ? "/assets/empty-folder-dark.png" : "/assets/empty-folder-light.png"}
           width={100}
           height={100}
           alt="empty"
         />
-        <div className="text-gray-400 font-bold">No Data</div>
+        <div className="text-gray-400 font-bold mt-4">No Data</div>
       </div>
     </div>
   );
