8b67df352402863ff87ddab53433eb8e017e8151
JoshVilla <villaearljoshua@gmail.com>
2025-03-09 20:17:16 +0800

    update metadata

--- app/metadata.ts
+++ app/metadata.ts
@@ -1,9 +1,9 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Library Management",
+  title: "Library Management by Earl Joshua Villa",
   description: "A modern library management system",
   icons: {
     icon: "/icons/books.png",
   },
-}; 
+};
